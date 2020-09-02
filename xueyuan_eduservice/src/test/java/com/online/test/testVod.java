9a2d1c70dc7c1c44dcdb3464d097f674365ef55b
lbrshadow1brli <lbrshadow1brli@qq.com>
2020-09-02 17:56:10 +0800

    9.2

--- xueyuan_eduservice/src/test/java/com/online/test/testVod.java
+++ xueyuan_eduservice/src/test/java/com/online/test/testVod.java
@@ -1,5 +1,8 @@
 package com.online.test;
 
+import com.aliyun.vod.upload.impl.UploadVideoImpl;
+import com.aliyun.vod.upload.req.UploadVideoRequest;
+import com.aliyun.vod.upload.resp.UploadVideoResponse;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.vod.model.v20170321.GetPlayInfoRequest;
@@ -11,23 +14,32 @@ import java.util.List;
 
 public class testVod {
 
-    public static void main(String[] args) throws ClientException {
-        //2 根据视频id 获取视频播放凭证
-        //创建初始化对象
-        DefaultAcsClient client = initObject.initVodClient("LTAI4GD68W9FjZRN8AEEFfVC", "gGCTbzi854i4jKz1Patp68fBNhjlMM");
+    public static void main(String[] args) {
 
-        //创建获取视频凭证request和response
-        GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
-        GetVideoPlayAuthResponse response = new GetVideoPlayAuthResponse();
+        String accessKeyId = "LTAI4GD68W9FjZRN8AEEFfVC";
+        String accessKeySecret = "gGCTbzi854i4jKz1Patp68fBNhjlMM";
+        String title = "VID_202009";
+        String fileName = "D:/VID_202009.mp4"; //文件路径名称
 
-        request.setVideoId("82907f65d95b44a5a024ea43e65d8a35");
+        //上传视频
+        UploadVideoRequest request = new UploadVideoRequest(accessKeyId, accessKeySecret, title, fileName);
 
-        response = client.getAcsResponse(request);
+        /* 可指定分片上传时每个分片的大小，默认为2M字节 */
+        request.setPartSize(2 * 1024 * 1024L);
+        /* 可指定分片上传时的并发线程数，默认为1，(注：该配置会占用服务器CPU资源，需根据服务器情况指定）*/
+        request.setTaskNum(1);
 
-        //播放凭证
-        System.out.print("PlayAuth = " + response.getPlayAuth() + "\n");
-        //VideoMeta信息
-        System.out.print("VideoMeta.Title = " + response.getVideoMeta().getTitle() + "\n");
+        UploadVideoImpl uploader = new UploadVideoImpl();
+        UploadVideoResponse response = uploader.uploadVideo(request);
+
+        if (response.isSuccess()) {
+            System.out.print("VideoId=" + response.getVideoId() + "\n");
+        } else {
+            /* 如果设置回调URL无效，不影响视频上传，可以返回VideoId同时会返回错误码。其他情况上传失败时，VideoId为空，此时需要根据返回错误码分析具体错误原因 */
+            System.out.print("VideoId=" + response.getVideoId() + "\n");
+            System.out.print("ErrorCode=" + response.getCode() + "\n");
+            System.out.print("ErrorMessage=" + response.getMessage() + "\n");
+        }
     }
 
     public static void getPlayUrl() throws ClientException {
@@ -53,5 +65,24 @@ public class testVod {
         //Base信息
         System.out.print("VideoBase.Title = " + response.getVideoBase().getTitle() + "\n");
 
+    }
+
+    public static void getPlayAuth() throws ClientException {
+        //2 根据视频id 获取视频播放凭证
+        //创建初始化对象
+        DefaultAcsClient client = initObject.initVodClient("LTAI4GD68W9FjZRN8AEEFfVC", "gGCTbzi854i4jKz1Patp68fBNhjlMM");
+
+        //创建获取视频凭证request和response
+        GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+        GetVideoPlayAuthResponse response = new GetVideoPlayAuthResponse();
+
+        request.setVideoId("82907f65d95b44a5a024ea43e65d8a35");
+
+        response = client.getAcsResponse(request);
+
+        //播放凭证
+        System.out.print("PlayAuth = " + response.getPlayAuth() + "\n");
+        //VideoMeta信息
+        System.out.print("VideoMeta.Title = " + response.getVideoMeta().getTitle() + "\n");
     }
 }
